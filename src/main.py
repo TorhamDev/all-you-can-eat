d45a4beff2af366b2a111fe7ca6aa90ad1fee47d
Torham <torhamdev@gmail.com>
2025-08-11 19:07:32 +0330

    ✨ Add root path to serve the index.html

--- src/main.py
+++ src/main.py
@@ -3,9 +3,15 @@ from fastapi import FastAPI, Depends
 from fastapi.responses import StreamingResponse
 from src.utils.files import file_generator
 from src.utils.parser import parse_size_to_byte
+from starlette.responses import FileResponse
 
 
 app = FastAPI(debug=True)
+
+
+@app.get("/")
+async def read_index():
+    return FileResponse("./src/templates/index.html")
 
 
 @app.get("/eat")
